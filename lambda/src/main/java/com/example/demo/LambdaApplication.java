4fbdda5b34b40210c9ab963f51d0244a28460488
Marcus Dev <51051052+DevMarcus007@users.noreply.github.com>
2021-12-16 19:58:17 -0300

    Lambda

---
+++ lambda/src/main/java/com/example/demo/LambdaApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LambdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LambdaApplication.class, args);
+		System.out.println("");
+		System.out.println("https://github.com/nyodinariai/S1-T2-BlueBank");
+		System.out.println("");
+		System.out.println("http://s1t2bluebank-env.eba-erixptxj.us-east-1.elasticbeanstalk.com");
+	}
+
+}
